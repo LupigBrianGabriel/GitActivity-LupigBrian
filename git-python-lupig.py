184dbfe68297c7e623ebbd2ffb4a453ac54b7840
LupigBrianGabriel <lupig.briangabriel@mscmarinduque.edu.ph>
2024-02-20 14:33:43 +0800

    My Educational Background added

--- git-python-lupig.py
+++ git-python-lupig.py
@@ -1,3 +1,6 @@
 print ("Brian GabrielO. Lupig")
 print ("Balogo, Boac, Marinduque")
 print ("August 06, 2002")
+print ("Marinduque State College")
+print ("Bachelor of Science in Information Technology")
+print ("2C")
